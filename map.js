43a31172880e440dd307072a7678661e5271c6b8
mollietaylor <mollie.taylor@gmail.com>
2015-02-21 21:28:39 -0500

    Adds Foursquare integration

--- map.js
+++ map.js
@@ -1,3 +1,53 @@
+// foursquare integration
+
+var govtIcon = L.AwesomeMarkers.icon({
+  icon: 'university',
+  prefix: 'fa',
+  markerColor: 'green'
+});
+
+var config = {
+  apiKey: 'EJAXVB3GXRY550WSHRBEVAHYKIKGHW0YXHQIVJVMMF3OLQID',
+  authUrl: 'https://foursquare.com/',
+  apiUrl: 'https://api.foursquare.com/'
+};
+
+var CLIENT_ID = 'EJAXVB3GXRY550WSHRBEVAHYKIKGHW0YXHQIVJVMMF3OLQID'
+var CLIENT_SECRET = 'OPOIMXAYAEIDLPH1E1GB2HDF4UKJVATA25R2LJVCFQX4GOZK'
+
+var API_ENDPOINT = 'https://api.foursquare.com/v2/venues/search' +
+  '?client_id=CLIENT_ID' + 
+  '&client_secret=CLIENT_SECRET' +
+  '&v=20130815' +
+  '&ll=33.74986,-84.39223' + 
+  '&radius=889' +
+  '&limit=50' +
+  '&intent=browse' +
+  // '&query=coffee' +
+  '&categoryId=4bf58dd8d48988d126941735' +
+  '&callback=?';
+
+var foursquarePlaces = L.layerGroup().addTo(map);
+
+$.getJSON(API_ENDPOINT
+  .replace('CLIENT_ID', CLIENT_ID)
+  .replace('CLIENT_SECRET', CLIENT_SECRET), function(result, status) {
+
+    if (status !== 'success') return alert('Request to Foursquare failed');
+
+    // transform venue result into map marker
+    for (var i = 0; i < result.response.venues.length; i++) {
+      var venue = result.response.venues[i];
+      var latlng = L.latLng(venue.location.lat, venue.location.lng);
+      var marker = L.marker(latlng, {
+        icon: govtIcon
+      })
+      .bindPopup('<strong><a href="https://foursquare.com/v/' + venue.id + '" target="_blank">' + venue.name + '</a></strong>')
+      .addTo(foursquarePlaces);
+    }
+
+  });
+
 
 // map
 
@@ -55,9 +105,10 @@ var landmarksLayer = L.geoJson(landmarks, {
 }).addTo(map);
 
 var overlayMaps = {
-  "MARTA Train Stations": MARTALayer,
-  "Historical Markers": historicalMarkersLayer,
-  "<span style='color: #8e44ad;'>▌</span>Landmarks": landmarksLayer
+  "<i class='fa fa-subway' style='color:#D43E2A'></i> MARTA Train Stations": MARTALayer,
+  "<i class='fa fa-subway' style='color:#38AADD'></i> Historical Markers": historicalMarkersLayer,
+  "<span style='color: #8e44ad;'>▌</span>Landmarks": landmarksLayer,
+  "<i class='fa fa-university' style='color:#72AF26'></i> Government Buildings <img src='images/foursquare-logomark.png' width='18' valign='bottom'>": foursquarePlaces
 };
 
 L.control.layers(null, overlayMaps, {
